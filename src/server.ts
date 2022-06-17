48c771c4ca4c583a97c7bad84ef17e5a07d4e357
zakiego <belummikir@gmail.com>
2022-06-18 00:16:02 +0800

    Fix: bugs host value

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import routes from "./routes";
 
 const init = async () => {
   const server: Server = Hapi.server({
-    port: 5001,
-    host: "localhost",
+    port: 5000,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
